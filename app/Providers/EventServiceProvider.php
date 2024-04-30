4cb70ea6b3cdae8ea3639b5ae32ab91d2e819b8d
Joe Bocock <47351766+JoeBocock@users.noreply.github.com>
2024-04-30 15:51:13 +0100

    Allow SignedIn event to login users

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,16 @@
 
 namespace App\Providers;
 
+use App\Enums\ClaimTypes;
+use App\Enums\SSOProtocol;
+use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Slides\Saml2\Events\SignedIn;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -25,7 +31,22 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Event::listen(SignedIn::class, function (SignedIn $event) {
+            $samlUser = $event->getSaml2User();
+
+            $user = User::updateOrCreate([
+                'mylogin_id' => $samlUser->getAttribute(ClaimTypes::MyLoginID->value)[0],
+            ], [
+                'name' => "{$samlUser->getAttribute(ClaimTypes::FirstName->value)[0]} {$samlUser->getAttribute(ClaimTypes::LastName->value)[0]}",
+                'email' => $samlUser->getAttribute(ClaimTypes::EmailAddress->value)[0],
+                'password' => Hash::make(Str::random()),
+                'last_saml_assertion' => $samlUser->getAttributes(),
+            ]);
+
+            auth()->login($user);
+
+            session()->replace(['last_login_protocol' => SSOProtocol::SAML->value]);
+        });
     }
 
     /**
