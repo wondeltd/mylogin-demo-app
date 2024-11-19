084fba85376e2013d578f7fe3b35d73f152c0883
Luke Broad <lbroad@wonde.com>
2024-11-19 11:04:54 +0000

    Update seeder to create SAML tenant for MyLogin local development

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        if(config('app.run-internal-mylogin-development-seeder')) {
+            $this->call([
+                InternalMyLoginDevelopmentSeeder::class,
+            ]);
+        }
     }
 }
